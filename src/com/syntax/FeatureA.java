25b221281859128f49a656a0616e2c48b377b3df
tasankuluulu <tasankuluulu@gmail.com>
2020-09-24 20:47:45 -0400

    FeatureA class added

---
+++ src/com/syntax/FeatureA.java
@@ -0,0 +1,8 @@
+package com.syntax;
+
+public class FeatureA {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/syntax01/AnotherClass.java
@@ -0,0 +1,7 @@
+package com.syntax01;
+
+public class AnotherClass {
+public static void main(String[] args) {
+	
+}
+}

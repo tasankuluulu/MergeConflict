5653b6f1df8ef2bf25893ce0d30abad426869ccd
tasankuluulu <tasankuluulu@gmail.com>
2020-09-24 21:29:09 -0400

    FeatureA class completed

--- src/com/syntax/FeatureA.java
+++ src/com/syntax/FeatureA.java
@@ -2,7 +2,8 @@ package com.syntax;
 
 public class FeatureA {
 	public static void main(String[] args) {
-		
+		System.out.println("Another logic after FeatureB class added 9:16 ");
+		System.out.println("FeatureA completed at 21:27");
 	}
 
 }

d775d2536aae7215e2dbd216ab65926d17851070
tasankuluulu <tasankuluulu@gmail.com>
2020-09-24 20:04:37 -0400

    git stash pop added

--- src/com/syntax/TestClass.java
+++ src/com/syntax/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("new version");
+		System.out.println("git stash pop");
 	}
 }
